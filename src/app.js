2e020331f4f58c97c49628348cdc8261943dc83e
Brayan2JZ <b.sredzinski92@gmail.com>
2024-08-09 23:37:50 +0000

    Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let exts = [".com", ".us", ".gov", ".org"];
+
+  function domainNameGen() {
+    let domainNames = [];
+
+    for (let pronoun of pronouns) {
+      for (let adj of adjs) {
+        for (let noun of nouns) {
+          for (let ext of exts) {
+            document.querySelector("#domainNameGenerator").innerHTML +=
+              "<li><h1>" + pronoun + adj + noun + ext + "</h1></li>";
+          }
+        }
+      }
+    }
+    return domainNames;
+  }
+
+  domainNameGen();
 };
